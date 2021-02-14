c38b46cec5ef3ac2e27a526b912ce887d3f06303
fivaz <31249205+fivaz@users.noreply.github.com>
2021-02-14 17:53:27 +0100

    font awesome installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MealPageComponent } from './meal/meal-page/meal-page.component';
 import { MealRowComponent } from './meal/meal-row/meal-row.component';
 import {HttpClientModule} from "@angular/common/http";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
