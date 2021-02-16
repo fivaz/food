6b2f5e9740f7ca34705662f3abfb21fe390c5c47
fivaz <31249205+fivaz@users.noreply.github.com>
2021-02-16 22:08:55 +0100

    fea - MealService function findAllFull, find and findIngredients created

--- src/app/meal/meal.service.ts
+++ src/app/meal/meal.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {Meal} from "./meal";
+import {Ingredient} from "../ingredient/ingredient";
 
 const API_URL = environment.apiUrl + '/meals';
 
@@ -17,15 +18,29 @@ export class MealService {
     return this.http.get<Meal[]>(API_URL);
   }
 
+  findAllFull() {
+    const params = new HttpParams().set('full', 'true');
+    return this.http.get<Meal[]>(API_URL, {params});
+  }
+
+  find(id: number) {
+    return this.http.get<Meal[]>(`${API_URL}/${id}`);
+  }
+
+  findIngredients(id: number) {
+    return this.http.get<Ingredient[]>(`${API_URL}/${id}/ingredients`);
+  }
+
   create(meal: Meal) {
+    console.log(meal);
     return this.http.post<Meal>(API_URL, meal);
   }
 
   edit(meal: Meal) {
-    return this.http.put<Meal>(API_URL + '/' + meal.id, meal);
+    return this.http.put<Meal>(`${API_URL}/${meal.id}`, meal);
   }
 
   delete(id: number) {
-    return this.http.delete(API_URL + '/' + id);
+    return this.http.delete(`${API_URL}/${id}`);
   }
 }
