ce10de85dd1f07e51e8e3d1c5079c1b8f35544f1
fivaz <31249205+fivaz@users.noreply.github.com>
2021-02-18 23:45:08 +0100

    wip - IngredientPage added table and data

--- src/app/ingredient/ingredient-page/ingredient-page.component.ts
+++ src/app/ingredient/ingredient-page/ingredient-page.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ingredient} from "../ingredient";
+import {IngredientService} from "../ingredient.service";
 
 @Component({
   templateUrl: './ingredient-page.component.html',
   styleUrls: ['./ingredient-page.component.css']
 })
 export class IngredientPageComponent implements OnInit {
+  ingredients: Ingredient[] = [];
+  columnsToDisplay = [
+    'name',
+    'unit',
+    'isCountable',
+    'price',
+    'quantity',
+    'actions',
+  ];
 
-  constructor() { }
+  constructor(private api: IngredientService) {
+  }
 
   ngOnInit(): void {
+    this.api.findAll().subscribe(ingredients => this.ingredients = ingredients);
   }
 
 }
--- src/app/ingredient/ingredient-page/ingredient-page.module.ts
+++ src/app/ingredient/ingredient-page/ingredient-page.module.ts
@@ -1,12 +1,22 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {IngredientPageComponent} from "./ingredient-page.component";
+import {MatCardModule} from "@angular/material/card";
+import {NavbarModule} from "../../shared/navbar/navbar.module";
+import {MatTableModule} from "@angular/material/table";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [IngredientPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    NavbarModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [IngredientPageComponent]
 })
