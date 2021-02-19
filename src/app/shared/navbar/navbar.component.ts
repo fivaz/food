340cdd285ff89cf283c7dccb73b0200aa8618d2a
fivaz <31249205+fivaz@users.noreply.github.com>
2021-02-19 11:47:00 +0100

    wip - side navbar with links

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'f-navbar',
@@ -7,9 +8,19 @@ import {Component, Input, OnInit} from '@angular/core';
 export class NavbarComponent implements OnInit {
   @Input() title: string = '';
 
-  constructor() {
+  showFiller = false;
+
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
+  }
+
+  goToMealsPage() {
+    this.router.navigateByUrl('/');
+  }
+
+  goToIngredientsPage() {
+    this.router.navigateByUrl('/ingredients');
   }
 }
--- src/app/shared/navbar/navbar.module.ts
+++ src/app/shared/navbar/navbar.module.ts
@@ -2,13 +2,21 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {NavbarComponent} from "./navbar.component";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatButtonModule} from "@angular/material/button";
+import {MatListModule} from "@angular/material/list";
 
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule
   ],
   exports: [NavbarComponent]
 })
