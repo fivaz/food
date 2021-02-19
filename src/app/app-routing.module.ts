44aae8690fa2192cef8d633cb477fcf2e00777e8
fivaz <31249205+fivaz@users.noreply.github.com>
2021-02-19 14:06:46 +0100

    ref - routes changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MealPageComponent} from "./meal/meal-page/meal-page.component";
 import {IngredientPageComponent} from "./ingredient/ingredient-page/ingredient-page.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 
 const routes: Routes = [
   {
     path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'meals',
     component: MealPageComponent,
   }, {
     path: 'ingredients',
